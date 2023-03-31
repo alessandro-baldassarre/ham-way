24e01c69b295bc190cbe44e0a9851f26d07277e0
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-03-31 13:52:27 +0200

    Add animations

--- main.js
+++ main.js
@@ -98,3 +98,29 @@ function lightMode() {
     themeButton.classList.replace("fa-sun", "fa-moon")
     localStorage.setItem("mode", "light")
 }
+
+// ~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 400
+})
+
+sr.reveal(".home__img")
+sr.reveal(".home__data", { origin: "bottom" })
+
+
+sr.reveal(".about__data", { origin: "left" })
+sr.reveal(".about__img", { origin: "right" })
+
+
+sr.reveal(".popular__card", { interval: 100 })
+
+sr.reveal(".recently__data", { origin: "left" })
+sr.reveal(".recently__img", { origin: "right" })
+
+sr.reveal(".newsletter")
+
+sr.reveal(".footer")
+
